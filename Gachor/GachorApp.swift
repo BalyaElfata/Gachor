1216d80af8b979948b46e4013a56094d92f7970e
Balya Elfata <balyaelfata@yahoo.com>
2024-04-24 14:06:56 +0700

    Initial Commit

---
+++ Gachor/GachorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GachorApp.swift
+//  Gachor
+//
+//  Created by Balya Elfata on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GachorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
