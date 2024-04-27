62b6c935930a0ebf63ec8d7a1386c263317427cc
Balya Elfata <balyaelfata@yahoo.com>
2024-04-27 21:39:41 +0700

    Added Anchors

--- AR/CustomARViewRepresentable.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct CustomARViewRepresentable: UIViewRepresentable {
-    func makeUIView(context: Context) -> some UIView {
-        return CustomARView()
-    }
-    
-    func updateUIView(_ uiView: UIViewType, context: Context) { }
-}
