766fa0ea157e774c6835b830913327406379c274
Balya Elfata <balyaelfata@yahoo.com>
2024-05-02 01:43:38 +0700

    adjust CardModel for future addition

--- Model/Card.swift
+++ Model/Card.swift
@@ -25,12 +25,14 @@ struct CardModel: Identifiable, Hashable {
     static func getCardData() -> [CardModel] {
         return [
             CardModel(name: "kak khoi", role: "Figure", rarity: "Legendary", imageName: "kak khoi", pattern: "pattern1"),
-            CardModel(name: "ko handy", role: "Design", rarity: "Super Rare", imageName: "ko handy", pattern: "pattern2"),
+            CardModel(name: "ko handy", role: "Figure", rarity: "Super Rare", imageName: "ko handy", pattern: "pattern2"),
             CardModel(name: "pak har", role: "Tech", rarity: "Rare", imageName: "pak har", pattern: "pattern3"),
-            CardModel(name: "kak athoya", role: "Skill", rarity: "Rare", imageName: "kak athoya", pattern: "pattern4"),
+            CardModel(name: "kak athoya", role: "Tech", rarity: "Rare", imageName: "kak athoya", pattern: "pattern4"),
             CardModel(name: "kak david", role: "Tech", rarity: "Rare", imageName: "kak david", pattern: "pattern5"),
             CardModel(name: "kak octa", role: "Tech", rarity: "Rare", imageName: "kak octa", pattern: "pattern6"),
             CardModel(name: "kak viona", role: "Tech", rarity: "Rare", imageName: "kak viona", pattern: "pattern7"),
+//            CardModel(name: "kak khoi", role: "Tech", rarity: "Rare", imageName: "kak khoi", pattern: "pattern8"),
+//            CardModel(name: "ko handy", role: "Tech", rarity: "Rare", imageName: "ko handy", pattern: "pattern9"),
         ]
     }
 }
